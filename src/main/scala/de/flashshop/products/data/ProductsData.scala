1cbf092ce6b4b29d4588eebe526ccc5cf94361a8
Dmitry Egorov <degorov86@gmail.com>
2014-08-16 21:58:14 +0400

    updated prices

--- src/main/scala/de/flashshop/products/data/ProductsData.scala
+++ src/main/scala/de/flashshop/products/data/ProductsData.scala
@@ -7,9 +7,9 @@ import MoneyImplicits._
 object ProductsData
 {
   val all = ProductDescriptionList.from(
-    ProductDescription("Starbound", 211820, 10.0),
-    ProductDescription("Civilization V", 8930,   10.0),
-    ProductDescription("Road Not Taken", 293740, 10.0),
-    ProductDescription("Call of Duty: Modern Warfare 3", 115300, 10.0)
+    ProductDescription("Starbound",                      211820, 10.0),
+    ProductDescription("Civilization V",                 8930,    5.0),
+    ProductDescription("Road Not Taken",                 293740, 10.0),
+    ProductDescription("Call of Duty: Modern Warfare 3", 115300, 50.0)
   )
 }
